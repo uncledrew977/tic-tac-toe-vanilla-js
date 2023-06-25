191a9fa907a5298005bd0b4c176c44d0413844de
Andrew <andrewokeke@macbook.mynetworksettings.com>
2023-06-25 19:11:11 -0400

    Finished 2/2 diagonal check for win.

--- js/Grid.js
+++ js/Grid.js
@@ -4,6 +4,7 @@ const MAX_X = 6;
 const MAX_Y = 5;
 export default class Grid {
     #cells
+
     constructor(gridElement) {
         this.#cells = createGameBoard(gridElement).map((cellElement, index) => {
             return new Cell(cellElement, index % 7, Math.floor(index / 7))
@@ -18,19 +19,19 @@ export default class Grid {
     }
 
     #getCellsByRow() {
-        return this.#cells.reduce((rows,cell) => {
+        return this.#cells.reduce((rows, cell) => {
             rows[cell.y] = rows[cell.y] || [];
             rows[cell.y][cell.x] = cell;
             return rows;
-        },[])
+        }, [])
     }
 
     #getCellsByColumns() {
-        return this.#cells.reduce((columns,cell) => {
+        return this.#cells.reduce((columns, cell) => {
             columns[cell.x] = columns[cell.x] || [];
             columns[cell.x][cell.y] = cell;
             return columns;
-        },[])
+        }, [])
     }
 
     canPlacePieceInRow(columnNumber) {
@@ -41,8 +42,8 @@ export default class Grid {
 
     getNextOpenSpotInColumn(columnNumber) {
         let cellsInColumn = this.#getAllCellsInAColumn(columnNumber);
-        for(let i = 0; i < cellsInColumn.length; i++) {
-            if(cellsInColumn[i].gamePiece == null) {
+        for (let i = 0; i < cellsInColumn.length; i++) {
+            if (cellsInColumn[i].gamePiece == null) {
                 return cellsInColumn[i];
             }
         }
@@ -57,20 +58,19 @@ export default class Grid {
     #checkForStraightWin(cells) {
         let didWin = false;
         cells.forEach((row) => {
-            for(let i = 0; i < row.length; i++) {
+            for (let i = 0; i < row.length; i++) {
                 let gamePieceOne = row[i].gamePiece;
-                if(gamePieceOne == null) continue;
+                if (gamePieceOne == null) continue;
                 let count = 1;
-                for(let j = i + 1; j < row.length; j++) {
+                for (let j = i + 1; j < row.length; j++) {
                     let gamePieceTwo = row[j].gamePiece;
-                    if(gamePieceTwo == null) break;
-                    else if(gamePieceOne.color === gamePieceTwo.color) {
+                    if (gamePieceTwo == null) break;
+                    else if (gamePieceOne.color === gamePieceTwo.color) {
                         count++;
-                        if(count === 4) {
+                        if (count === 4) {
                             didWin = true;
                         }
-                    }
-                    else break;
+                    } else break;
                 }
             }
         })
@@ -78,22 +78,25 @@ export default class Grid {
     }
 
     #checkForDiagonalWin(cells) {
+        return this.#checkForwardDiagonal(cells) || this.#checkBackwardDiagonal(cells);
+    }
+
+    #checkForwardDiagonal(cells) {
         let didWin = false;
         cells.forEach((row) => {
-            for(let i = 0; i < row.length; i++) {
+            for (let i = 0; i < row.length; i++) {
                 let cell = row[i];
                 let gamePieceOne = cell.gamePiece;
-                if(gamePieceOne == null) continue;
+                if (gamePieceOne == null) continue;
                 let nextX = cell.x + 1;
                 let nextY = cell.y + 1;
                 let count = 1;
-                while(nextX <= MAX_X && nextY <= MAX_Y) {
+                while (nextX <= MAX_X && nextY <= MAX_Y) {
                     let gamePieceTwo = cells[nextX][nextY].gamePiece
-                    if(gamePieceTwo == null) break;
-                    if(gamePieceTwo.color === gamePieceOne.color){
+                    if (gamePieceTwo == null) break;
+                    if (gamePieceTwo.color === gamePieceOne.color) {
                         count++;
-                        console.log("MATCH!")
-                        if(count === 4) {
+                        if (count === 4) {
                             didWin = true;
                             break;
                         }
@@ -108,12 +111,46 @@ export default class Grid {
 
         return didWin;
     }
+
+    #checkBackwardDiagonal(cells) {
+        let didWin = false;
+        cells.forEach((row) => {
+            for (let i = 0; i < row.length; i++) {
+                let cell = row[i];
+                let gamePieceOne = cell.gamePiece;
+                if (gamePieceOne == null) continue;
+                let nextX = cell.x + 1;
+                let nextY = cell.y - 1;
+                let count = 1;
+                while (nextX <= MAX_X && nextY >= 0) {
+                    let gamePieceTwo = cells[nextX][nextY].gamePiece
+                    if (gamePieceTwo == null) break;
+                    if (gamePieceTwo.color === gamePieceOne.color) {
+                        count++;
+                        if (count === 4) {
+                            didWin = true;
+                            break;
+                        }
+                        nextX++;
+                        nextY--;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        })
+
+        return didWin;
+    }
+
+
+
 }
 
 
 function createGameBoard(gridElement) {
     let cellElements = [];
-    for(let i = 0; i < 42; i++) {
+    for (let i = 0; i < 42; i++) {
         let cellElement = document.createElement("div");
         cellElement.classList.add("game-cell");
         cellElement.setAttribute("column", `${Math.floor(i % 7)}`);
